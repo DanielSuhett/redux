6024d1366444dfb9a521094f0982d4debf934162
DanielSuhett <danysuhett@hotmail.com>
2021-04-26 00:37:47 -0300

    index page to boilerplate

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,63 @@
-import * as S from "./styles";
+import {
+  Button,
+  Flex,
+  Heading,
+  useColorMode,
+  useColorModeValue
+} from "@chakra-ui/react";
+import { SiGithub, SiLinkedin, SiTwitter } from "react-icons/si";
+import { IoMdMoon, IoMdSunny } from "react-icons/io";
 
-const Main = () => (
-  <S.Wrapper>
-    <S.Title>Maps</S.Title>
-  </S.Wrapper>
-);
+const Main = () => {
+  const styles = {
+    icons: {
+      fontSize: "24px"
+    }
+  };
+  const { toggleColorMode } = useColorMode();
+  const formBackround = useColorModeValue("gray.100", "gray.700");
+  const ToggleIcon = useColorModeValue(IoMdSunny, IoMdMoon);
+  const Background = useColorModeValue(
+    "linear(#fff, #007A7A)",
+    "linear-gradient(#2D3748, #007A7A)"
+  );
+
+  const openLink = (link: string) => window.open(link);
+
+  return (
+    <Flex
+      height="100vh"
+      bgGradient={Background}
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex direction="column" background={formBackround} p={12} rounded={6}>
+        <Heading mb={6} colorScheme="teal">
+          Daniels Boilerplate
+        </Heading>
+        <Button mb={12} colorScheme="teal" onClick={toggleColorMode}>
+          <ToggleIcon style={styles.icons} />
+        </Button>
+        <Flex justifyContent="space-around">
+          <Button onClick={() => openLink("https://github.com/DanielSuhett")}>
+            <SiGithub style={styles.icons} />
+          </Button>
+          <Button
+            onClick={() =>
+              openLink("https://www.linkedin.com/in/daniel-suhett/")
+            }
+          >
+            <SiLinkedin style={styles.icons} />
+          </Button>
+          <Button
+            onClick={() => openLink("https://twitter.com/danielsuhett99")}
+          >
+            <SiTwitter style={styles.icons} />
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
 
 export default Main;
--- src/components/Main/tests/test.tsx
+++ src/components/Main/tests/test.tsx
@@ -1,11 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import { ThemeProvider, theme } from "@chakra-ui/react";
 
 import Main from "..";
 
 describe("<Main />", () => {
   it("should render the heading ", () => {
-    render(<Main />);
+    render(
+      <ThemeProvider theme={theme}>
+        <Main />
+      </ThemeProvider>
+    );
 
-    expect(screen.getByRole("heading", { name: /maps/i }));
+    expect(screen.getByRole("heading", { name: /Daniels Boilerplate/i }));
   });
 });
